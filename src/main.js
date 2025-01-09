8a59c134a37d03551198cc20bd4665f670a09d97
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-09 14:32:35 +0900

    ✨取得した投稿をフィルタリング

--- src/main.js
+++ src/main.js
@@ -49,15 +49,32 @@ function showLoading() {
   }, 1000);
 }
 
+// 入力を検知して投稿のフィルター
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
 // 初期データを表示
 showPosts();
 
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  console.log(clientHeight);
-
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
+
+filter.addEventListener("input", filterPosts);
