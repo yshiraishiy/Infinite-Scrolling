f522ea1d7374676a8eb2a5793e925bf37e3d88ce
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-09 13:58:01 +0900

    ✨APIから最初の投稿を取得する

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const postsContainer = document.getElementById("posts-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 3;
+let page = 1;
+
+// API/POST
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+// 取得したデータを画面に表示
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+// 初期データを表示
+showPosts();
